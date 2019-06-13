a68841f667988d223453838e9f828e25e9a2f6f0
RobinChen <robin.chen@wondalink.com>
2019-06-13 12:01:27 +0800

    update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
-autoreconf --install --force && ./configure
+
+if [ -x "`which autoreconf 2>/dev/null`" ] ; then
+	exec autoreconf -ivf
+fi
+
+aclocal -I m4  && \
+	autoheader && \
+	libtoolize && \
+	autoconf   && \
+	automake --add-missing --force-missing --copy
